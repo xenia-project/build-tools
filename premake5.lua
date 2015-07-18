a08743c52acf26dce1d9676a3a0fb3abd81cd29a
Ben Vanik <ben.vanik@gmail.com>
2015-07-18 15:05:40 -0700

    Initial build_tools.

---
+++ premake5.lua
@@ -0,0 +1,4 @@
+include("scripts/build_paths.lua")
+include("scripts/force_compile_as_cc.lua")
+include("scripts/platform_files.lua")
+include("scripts/test_suite.lua")
---
+++ third_party/gflags.lua
@@ -0,0 +1,31 @@
+group("third_party")
+project("gflags")
+  uuid("e319da87-75ed-4517-8f65-bd25e9cc02a3")
+  kind("StaticLib")
+  language("C++")
+  links({
+  })
+  defines({
+    "PATH_SEPARATOR=%%27\\\\%%27",
+    "GFLAGS_DLL_DECL=",
+    "GFLAGS_DLL_DEFINE_FLAG=",
+    "GFLAGS_DLL_DECLARE_FLAG=",
+    "_LIB",
+  })
+  includedirs({
+    "gflags/src/windows",
+    "gflags/src",
+  })
+  files({
+    "gflags/src/gflags.cc",
+    "gflags/src/gflags_completions.cc",
+    "gflags/src/gflags_reporting.cc",
+    "gflags/src/mutex.h",
+    "gflags/src/util.h",
+    "gflags/src/windows/config.h",
+    "gflags/src/windows/gflags/gflags.h",
+    "gflags/src/windows/gflags/gflags_completions.h",
+    "gflags/src/windows/gflags/gflags_declare.h",
+    "gflags/src/windows/port.cc",
+    "gflags/src/windows/port.h",
+  })
